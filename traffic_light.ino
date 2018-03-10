bc2d3879f2da27ce45565d043b8632b9d5c553be
MohamedNageh50 <36593555+MohamedNageh50@users.noreply.github.com>
2018-03-10 23:48:01 +0200

    Add files via upload

---
+++ traffic_light.ino
@@ -0,0 +1,39 @@
+
+int red=13;
+int yellow=12;
+int green=11;
+
+
+void setup() {
+  pinMode(red,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(green,OUTPUT);
+  
+
+}
+
+void loop() {
+
+digitalWrite(red,HIGH);
+digitalWrite(yellow,LOW);
+digitalWrite(green,LOW);
+delay(3000);
+digitalWrite(yellow,HIGH);
+digitalWrite(red,LOW);
+digitalWrite(green,LOW);
+delay(3000);
+digitalWrite(green,HIGH);
+digitalWrite(red,LOW);
+digitalWrite(yellow,LOW);
+delay(3000);
+
+
+
+
+
+
+
+  
+  
+
+}
