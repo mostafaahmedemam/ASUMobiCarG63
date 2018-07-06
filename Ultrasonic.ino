29414d8242e440274d8387c8c82c479549460d1f
MostafaSherif23 <36202473+MostafaSherif23@users.noreply.github.com>
2018-07-06 14:21:41 +0200

    Add files via upload

---
+++ Ultrasonic.ino
@@ -0,0 +1,31 @@
+const int trigger=12;
+const int echo=11;
+const int motor=9;
+long duration,distance;
+void setup(){
+  pinMode(trigger,OUTPUT);
+  pinMode(echo,INPUT);
+  pinMode(motor,OUTPUT);
+  
+}
+void loop(){
+  digitalWrite(trigger,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger,LOW);
+  duration=pulseIn(echo,HIGH);
+  distance=0.034*duration/2;
+  Serial.println(distance);
+  if (distance >10){
+  analogWrite(motor,255);
+  
+}
+else if(distance<=10){
+  analogWrite(motor,0);
+  
+}
+  
+  
+}
+
